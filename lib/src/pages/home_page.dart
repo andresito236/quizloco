309fbc4748a7e6dc5acecd6306c40ab6476fd5d4
Derys Rodriguez <derysdominguez@gmail.com>
2023-08-26 23:11:28 -0600

    edit home page

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -2,33 +2,41 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:quizloco/src/constants/routes.dart';
 import 'package:quizloco/src/utils/app_bar_maker.dart';
-import 'package:quizloco/src/widgets/my_button.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   final user = FirebaseAuth.instance.currentUser;
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMaker(context, title: "Inicio"),
-      body: Center(child: Column(
-        children: [
-          Text("Bienvenido " + (user?.email ?? "UNKNOWN"), ),
-          MyButton(onTap: (){
-            Navigator.pushNamed(context, MyRoutes.createTest.name);
-          }, message: "Crea un test"),
-          MyButton(onTap: () {
-            Navigator.pushNamed(context, MyRoutes.myTests.name);
-          }, message: "Mis tests",),
-          MyButton(onTap: () {
-            Navigator.pushNamed(context, MyRoutes.tests.name);
-          }, message: "Todos los tests"),
-          
-        ],
-        )),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Bienvenido, ${user?.email ?? 'UNKNOWN'}",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, MyRoutes.myTests.name);
+              },
+              child: Text('Revisa qué tests has creado'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, MyRoutes.tests.name);
+              },
+              child: Text('¡Atrévete y realiza un intento en cualquier test!'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
